3d6d64809ea2933c4b32291706165fe83683ad3c
Diego Nonato <nonatodiego@hotmail.com>
2020-07-04 23:37:16 -0300

    att pagina create e att de rotas

--- routes.js
+++ routes.js
@@ -14,6 +14,23 @@ routes.get('/instructors/create', function(req,res){
   return res.render("instructors/create")
 })
 
+routes.post('/instructors', function(req,res){
+  //req.query
+  //req.body
+
+ const keys = Object.keys(req.body)
+
+ for(key of keys) {
+   //req.body.key == ""
+   if(req.body[key] == ""){
+     return res.send('Por favor, preencha todos os campos!')
+   }
+ }
+
+ return res.send(req.body)
+ 
+})
+
 routes.get('/members', function(req,res){
   return res.send("members")
 })
